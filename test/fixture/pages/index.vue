9da53438239d8e81782158f7242630e65e633caf
Alexander Kushi-Willis <ackw.kushi@gmail.com>
2018-03-31 13:48:28 -0400

    ready for first release

--- test/fixture/pages/index.vue
+++ test/fixture/pages/index.vue
@@ -1,11 +1,23 @@
 <template>
 <div>
     Works!
+    <pre>{{ $root.platform_os }}</pre>
+    <pre>{{ os }}</pre>
+    <pre>{{ $root.$platform }}</pre>
+
 </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      os: null
+    }
+  },
+  mounted() {
+    this.os = this.$root.$platform.os
+    this.$root.platform_os = 'haha'
+  }
 }
 </script>
